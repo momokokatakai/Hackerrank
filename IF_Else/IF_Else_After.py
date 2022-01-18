1f108b32f5b49573e5d0310e38f7405f087089f6
momokokatakai <97958574+momokokatakai@users.noreply.github.com>
2022-01-18 21:43:35 +0900

    Create IF_Else_After.py

---
+++ IF_Else/IF_Else_After.py
@@ -0,0 +1,35 @@
+# Task
+# Given an integer, , perform the following conditional actions:
+
+# If  is odd, print Weird
+# If  is even and in the inclusive range of  to , print Not Weird
+# If  is even and in the inclusive range of  to , print Weird
+# If  is even and greater than , print Not Weird
+
+
+#!/bin/python
+
+import math
+import os
+import random
+import re
+import sys
+
+if __name__ == '__main__':
+    n = int(raw_input().strip())
+
+
+    if n % 2 == 1:
+        print('Weird')
+    
+    if n % 2 == 0:
+        if  n >= 2 and n <= 5:
+            print('Not Weird')
+        elif n >= 6 and n <= 20:
+            print('Weird')
+        else:
+            print('Not Weird') 
+
+
+
+
