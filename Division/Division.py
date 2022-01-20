ee837dd22dc52b1368fa5bbe2830cfe3d8ad9c0d
momokokatakai <97958574+momokokatakai@users.noreply.github.com>
2022-01-20 21:19:05 +0900

    Create Division.py

---
+++ Division/Division.py
@@ -0,0 +1,13 @@
+from __future__ import division
+
+if __name__ == '__main__':
+    a = int(raw_input())
+    b = int(raw_input())
+
+print (a//b)
+
+print (a/b)
+
+
+# 🐻割り算！
+# /は普通の割り算。//は、整数部分だけを返す！
